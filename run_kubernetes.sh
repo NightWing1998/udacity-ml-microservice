d9cf6206fa9b2f9fe6f78fb1b493673e6c365e2b
NightWing1998 <dhruvil.s@somaiya.edu>
2020-05-11 13:00:30 +0530

    updated(run_kubernetes.sh): added commands to automate the run

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=dhruvilshah98/udacity-nanoproj-ml-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservice --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/ml-microservice 8000:80
